c59ddd1ca6047f022d273d2f674aac58f310d18b
prsana2019 <technology1032@gmail.com>
2021-05-05 21:57:04 +0530

    initial commit

---
+++ app/src/main/java/com/medical/medicalreport/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.medical.medicalreport
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000) // 3000 is the delayed time in milliseconds.
+    }
+}
+
