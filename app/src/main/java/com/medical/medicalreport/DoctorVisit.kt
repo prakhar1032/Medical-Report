f21ceea468d86b7e5f2b2458dd2257d3b81c17ea
sangyan11 <sangyanbhagi@gmail.com>
2021-05-08 19:23:37 +0530

    commit5

---
+++ app/src/main/java/com/medical/medicalreport/DoctorVisit.kt
@@ -0,0 +1,11 @@
+package com.medical.medicalreport
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoctorVisit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor_visit)
+    }
+}
