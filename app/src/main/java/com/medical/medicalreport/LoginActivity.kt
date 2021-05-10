e6997f46316f5973edb38af9d3a51bd588da374a
sangyan11 <sangyanbhagi@gmail.com>
2021-05-10 16:09:06 +0530

    commit 6

--- app/src/main/java/com/medical/medicalreport/LoginActivity.kt
+++ app/src/main/java/com/medical/medicalreport/LoginActivity.kt
@@ -1,15 +1,72 @@
 package com.medical.medicalreport
 
+import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+
+private var Login_Button: Button?=null
+private var SignIn_Button:Button?=null
+private var user_email:EditText?=null
+private var user_password:EditText?=null
+private var firebaseAuth:FirebaseAuth?=null
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        Login_Button = findViewById<Button>(R.id.login)
+        SignIn_Button = findViewById<Button>(R.id.signup)
+        user_email = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        user_password = findViewById<EditText>(R.id.editTextTextPassword)
+        firebaseAuth= FirebaseAuth.getInstance()
+
+
     }
 
-    fun login(view: View) {}
-    fun signup(view: View) {}
-}
+    fun login(view: View) {
+
+        LoginUser()
+
+    }
+
+    private fun LoginUser() {
+        var email_text = user_email?.text.toString()
+        var password_text = user_password?.text.toString()
+  if(TextUtils.isEmpty(email_text) || TextUtils.isEmpty(password_text)){
+      Toast.makeText(this," This field cannot be empty",Toast.LENGTH_SHORT).show()
+  }
+    else{
+        firebaseAuth?.signInWithEmailAndPassword(email_text,password_text)?.addOnCompleteListener(object : OnCompleteListener<AuthResult> {
+            override fun onComplete(task: Task<AuthResult>) {
+                if (task.isSuccessful) {
+
+                    Toast.makeText(applicationContext, "Login Successfully", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this@LoginActivity,MainActivity::class.java))
+                }
+                else{
+                    val error = task.exception?.message
+                    Toast.makeText(applicationContext, "Error! $error", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+
+        })
+
+
+    }
+
+    fun signup(view: View) {
+
+    }
+}}
