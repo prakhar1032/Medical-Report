aa870b83770c0c7f164dc2d59f6a437bfaf018ca
prsana2019 <technology1032@gmail.com>
2021-05-10 18:57:28 +0530

    initial Commit 7

--- app/src/main/java/com/medical/medicalreport/SignUp.kt
+++ app/src/main/java/com/medical/medicalreport/SignUp.kt
@@ -1,11 +1,17 @@
 package com.medical.medicalreport
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+    }
+
+    fun signup2(view: View) { val intent =Intent(this,LoginActivity::class.java)
+    startActivity(intent)
     }
 }
