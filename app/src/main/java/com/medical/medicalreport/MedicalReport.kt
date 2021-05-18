a648d2c07a1916806b3e7ad6f76c3844b637a3cd
sangyan11 <sangyanbhagi@gmail.com>
2021-05-18 20:53:17 +0530

    commit 15

--- app/src/main/java/com/medical/medicalreport/MedicalReport.kt
+++ app/src/main/java/com/medical/medicalreport/MedicalReport.kt
@@ -1,17 +1,77 @@
 package com.medical.medicalreport
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 
 class MedicalReport : AppCompatActivity() {
+    val uriTxt :TextView?=null
+    val PDF : Int = 0
+    val DOCX : Int = 1
 
+    lateinit var uri : Uri
+    lateinit var mStorage : StorageReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_medical_report)
 
-
+ val upload_pdf = findViewById<Button>(R.id.upload_pdf)
+ val upload_docx = findViewById<Button>(R.id.upload_docx)
+        mStorage = FirebaseStorage.getInstance().getReference("Uploads")
+        upload_pdf.setOnClickListener{
+            val intent = Intent()
+            intent.setType ("pdf/*")
+            intent.setAction(Intent.ACTION_GET_CONTENT)
+            startActivityForResult(Intent.createChooser(intent, "Select PDF"), PDF)
 
         }
+   upload_docx.setOnClickListener{
+           view: View? -> val intent = Intent()
+       intent.setType ("docx/*")
+       intent.setAction(Intent.ACTION_GET_CONTENT)
+       startActivityForResult(Intent.createChooser(intent, "Select DOCX"), DOCX)
+   }
+
+        }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PDF) {
+                uri = data!!.data!!
+                uriTxt?.text = uri.toString()
+                upload ()
+            }
+            else if (requestCode == DOCX) {
+                uri = data!!.data!!
+                uriTxt?.text = uri.toString()
+                upload ()
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
+
+    private fun upload() {
+        var mReference = uri.lastPathSegment?.let { mStorage.child(it) }
+        try {
+            mReference?.putFile(uri)!!.addOnSuccessListener {
+                    taskSnapshot: UploadTask.TaskSnapshot? -> var url = taskSnapshot!!
+                val dwnTxt = findViewById<View>(R.id.dwnTxt) as TextView
+                dwnTxt.text = url.toString()
+                Toast.makeText(this, "Successfully Uploaded :)", Toast.LENGTH_LONG).show()
+            }
+        }catch (e: Exception) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+        }
+    }
+}
+
